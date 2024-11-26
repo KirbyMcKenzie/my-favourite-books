c3d13e95f506c38696d8ca369a35e0e6d1c80fdb
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2024-11-25 21:05:20 -0500

    Convert PNG icons to be SVG components and delete initial project SVGs

---
+++ src/app/components/Icons/ArrowDown/ArrowDown.jsx
@@ -0,0 +1,18 @@
+const ArrowDown = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    fill="none"
+    viewBox="0 0 20 20"
+  >
+    <path
+      fill="#757575"
+      fillRule="evenodd"
+      d="M9.854 13.354a.5.5 0 0 1-.708 0l-6-6a.5.5 0 1 1 .708-.708L9.5 12.293l5.646-5.647a.5.5 0 0 1 .708.708z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+export default ArrowDown;
